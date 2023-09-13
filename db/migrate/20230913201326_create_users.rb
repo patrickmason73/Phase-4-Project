60080c6ebee96df48fd2e83366c3726900ea6d2e
Patrick Mason <patrickmason73@gmail.com>
2023-09-13 18:14:16 -0500

    created migrations, edited models + added category model for has_many relationships

--- db/migrate/20230913201326_create_users.rb
+++ db/migrate/20230913201326_create_users.rb
@@ -1,7 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-
+      t.string  :username
+      t.string  :password_digest
+      t.text    :bio
+      t.string  :profile_pic_url
       t.timestamps
     end
   end
--- db/migrate/20230913201607_create_post_tags.rb
+++
@@ -1,8 +0,0 @@
-class CreatePostTags < ActiveRecord::Migration[7.0]
-  def change
-    create_table :post_tags do |t|
-
-      t.timestamps
-    end
-  end
-end
