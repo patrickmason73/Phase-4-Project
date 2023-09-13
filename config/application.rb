bbdc954be6fb5f20f1ce909b57f95f0c80d1c44f
Patrick Mason <patrickmason73@gmail.com>
2023-09-13 15:44:25 -0500

    generated models, frontend client, added gems

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,14 @@ module Phase4Project
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.api_only = true
 
+    # Adding back cookies and session middleware
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
+    # Use SameSite=Strict for all cookies to help protect against CSRF
+    config.action_dispatch.cookies_same_site_protection = :strict
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
